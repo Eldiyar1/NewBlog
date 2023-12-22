49c813c416d55a339615f908fce413a0fbcb6ae8
Eldiyar1 <mc.oks1ne@gmail.com>
2023-12-23 01:35:10 +0600

    lesson 7

--- post/urls.py
+++ post/urls.py
@@ -5,7 +5,8 @@ from post import views
 urlpatterns = [
     path('', views.main_view),
     path('products/', views.products_view),
-    path('product/<int:product_id>/', views.product_detail_view, name='product_detail'),
+    path('products/<int:product_id>/', views.product_detail_view, name='product_detail'),
+    path('products/<int:product_id>/update/', views.product_update_view),
     path('products/create/', views.product_create),
     path('categories/', views.categories_view),
     path('categories/create/', views.category_create_view),
--- post/views.py
+++ post/views.py
@@ -1,5 +1,7 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 
+from blog import settings
 from .forms import ProductCreateForm, ProductCreateForm2, CommentCreateForm, CategoryCreateForm
 from .models import Product, HashTag, Category, Comment
 
@@ -13,8 +15,41 @@ def products_view(request):
     if request.method == 'GET':
         print(request.user)
         products = Product.objects.all()
+        search = request.GET.get('search')
+        order = request.GET.get('order')
+
+        if search:
+            # products = products.filter(title__icontains=search) | products.filter(content__icontains=search)
+            products = products.filter(
+                Q(title__icontains=search) |
+                Q(content__icontains=search)
+            )
+
+        if order == 'title':
+            products = products.order_by('title')
+        elif order == '-title':
+            products = products.order_by('-title')
+        elif order == 'created_at':
+            products = products.order_by('created_at')
+        elif order == '-created_at':
+            products = products.order_by('-created_at')
+
+        max_page = products.__len__() / settings.PAGE_SIZE
+
+        if round(max_page) < max_page:
+            max_page = round(max_page) + 1
+        else:
+            max_page = round(max_page)
+
+        page = int(request.GET.get('page', 1))
+
+        start = (page - 1) * settings.PAGE_SIZE
+        end = page * settings.PAGE_SIZE
+
+        products = products[start:end]
         context = {
             "products": products,
+            "pages": range(1, max_page + 1)
         }
         return render(request, 'products/products.html', context=context)
 
@@ -34,11 +69,29 @@ def product_detail_view(request, product_id):
         form = CommentCreateForm(request.POST, request.FILES)
         if form.is_valid():
             Comment.objects.create(product_id=product_id, **form.cleaned_data)
-            return redirect('product_detail', product_id=product_id)
+            return redirect(f'/product/{product_id}/')
         context = {
             'form': form,
         }
         return render(request, 'products/product_detail.html', context)
+
+
+def product_update_view(request, product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist:
+        return render(request, 'errors/404.html')
+    if request.method == 'GET':
+        context = {
+            "form": ProductCreateForm2(instance=product)
+        }
+        return render(request, 'products/product_update.html', context)
+    elif request.method == 'POST':
+        form = ProductCreateForm2(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/products/{product.id}/')
+        return render(request, 'products/product_update.html', {"form": form})
 
 
 def product_create(request):
